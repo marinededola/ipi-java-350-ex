8e56b969c2e792366b3003629e72c8666d98801c
marine <dedola.marine@hotmail.com>
2021-02-10 13:16:39 +0100

    Debut javadoc getNbRtt

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -59,6 +59,11 @@ public class Employe {
         return getNbRtt(LocalDate.now());
     }
 
+    /**
+     *
+     * @param d
+     * @return
+     */
     public Integer getNbRtt(LocalDate d){
         int i1 = d.isLeapYear() ? 365 : 366;int var = 104;
         switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()){
