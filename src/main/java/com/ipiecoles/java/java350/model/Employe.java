55d70d4f89724b32051ee7823da73df49400b259
marine <dedola.marine@hotmail.com>
2021-02-22 15:08:04 +0100

    TDD tests augmenterSalaire

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -121,7 +121,7 @@ case SATURDAY:var = var + 1;
     }
 
     //Augmenter salaire
-    //public void augmenterSalaire(double pourcentage){}
+    public void augmenterSalaire(double pourcentage){}
 
     public Long getId() {
         return id;
